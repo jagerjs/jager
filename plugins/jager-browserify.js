e07d01ee08997518d7484e50f4063c24fbe08c1d
Tim <me@justim.net>
2015-04-03 16:54:10 +0200

    Add watchify to browserify builds

--- plugins/jager-browserify.js
+++ plugins/jager-browserify.js
@@ -3,6 +3,7 @@
 
 var async = require('async');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var babelify = require('babelify');
 var extend = require('util')._extend;
 
@@ -12,9 +13,18 @@ var babelOptions = {
 	blacklist: ['useStrict'],
 };
 
-function processBrowserify(options, addDependency, file, cb) {
+var _instanceCache = {};
+
+function getBrowserify(options, file) {
 	var debug = !!options.sourceMap;
-	var b = browserify({ debug: debug });
+	var b = browserify({
+		debug: debug,
+		detectGlobals: false,
+		insertGlobals: true,
+		cache: {},
+		packageCache: {},
+		fullPaths: true
+	});
 
 	if (options.babel) {
 		b.transform(babelify.configure(babelOptions));
@@ -22,9 +32,27 @@ function processBrowserify(options, addDependency, file, cb) {
 
 	b.require(file.filename(), { entry: true });
 
-	b.on('file', addDependency);
+	return b;
+}
 
-	b.bundle(function(err, result) {
+var phr = require('pretty-hrtime');
+
+function processBrowserify(options, addDependency, file, cb) {
+	var b;
+
+	var s = process.hrtime();
+	if (!_instanceCache[file.filename()]) {
+		b = getBrowserify(options, file);
+		b.on('file', addDependency);
+
+		if (options.watch) {
+			_instanceCache[file.filename()] = watchify(b);
+		} else {
+			_instanceCache[file.filename()] = b;
+		}
+	}
+
+	_instanceCache[file.filename()].bundle(function(err, result) {
 		if (err) {
 			cb(err);
 		} else {
@@ -42,6 +70,10 @@ module.exports = function(options) {
 
 		function addDependency(dependency) {
 			that.jagerSrcDependencies = (that.jagerSrcDependencies || []).concat([dependency]);
+		}
+
+		if (this.watch) {
+			options.watch = true;
 		}
 
 		async.map(files, processBrowserify.bind(null, options, addDependency), cb);
