a5b952a43abfb740f62e0019cf6049e8614e0317
Tim <me@justim.net>
2016-12-09 10:41:34 +0100

    Add sass plugin

---
+++ plugins/jager-sass.js
@@ -0,0 +1,55 @@
+
+/**
+ * Process sass/scss files
+ *
+ * Process sass/scss file into css files
+ *
+ * **API**: `('sass'[, options])`
+ *
+ * - `options`: See [sass options] for all the available options
+ *
+ * [sass options]: https://github.com/sass/node-sass#options
+ */
+
+'use strict';
+
+var path = require('path');
+var extend = require('util')._extend;
+
+var async = require('async');
+var sass = require('node-sass');
+
+var __root = process.cwd();
+
+function compileSass(context, options, file, cb) {
+	var basePath = options.basePath || __root;
+
+	var sassOptions = {
+		includePaths: [basePath],
+		filename: path.relative(__root, file.filename()),
+		indentedSyntax: true,
+	};
+
+	extend(sassOptions, options);
+
+	sass.render({
+		file: file.filename(),
+		// data: file.contents(),
+	}, function(err, result) {
+		if (err) {
+			cb(err);
+		} else {
+			context.addDependency(result.stats.includedFiles);
+			file.buffer(result.css);
+			cb(null, file);
+		}
+	});
+}
+
+module.exports = function(rawOptions) {
+	var options = rawOptions || {};
+
+	return function sass(files, cb) {
+		async.map(files, compileSass.bind(null, this, options), cb);
+	};
+};
