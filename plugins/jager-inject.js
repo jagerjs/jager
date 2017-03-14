77dc7da353674b36c66338c758b8c1bee1c57824
Tim <me@justim.net>
2017-03-14 18:06:25 +0100

    Several improvements to the inject plugin
    - Better handling of multiple matching on a single line
    - Handle multiple html based on a glob
    - Filtered injects, possible to inject single files

--- plugins/jager-inject.js
+++ plugins/jager-inject.js
@@ -7,41 +7,42 @@
 
 var path = require('path');
 
-var _ = require('lodash');
+var async = require('async');
 var minimatch = require('minimatch');
+
+var __root = process.cwd();
 
 var templates = {
 	'.js': '<script src="{url}"></script>',
 	'.css': '<link rel="stylesheet" href="{url}">',
+	'.img': '<img src="{url}">',
 };
 
-function regexSafe(text) {
-	return text.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-}
-
-function findHtmlFile(files, glob) {
-	return _.find(files, function(file) {
+function findHtmlFiles(files, glob) {
+	return files.filter(function(file) {
 		return minimatch(file.filename(), glob);
 	});
 }
 
-function injectSources(context, base, files, htmlFile, cb) {
+function injectSourcesForFile(context, base, srcBase, files, htmlFile, cb) {
 	var manifest = context.getManifest(base).filter(function(file) {
 		return file.file.filename() !== htmlFile.filename();
 	});
 
-	var filesInjected = false;
 	var content = htmlFile.contents();
-	var pattern = /<!-- ?inject:(\w+) ?-->/g;
-	var urlPatterm = /{url}/g;
+	var pattern = /<!-- ?inject:(\w+)(\(([^)]+)\))? ?-->/g;
+	var urlPattern = /{url}/g;
 	var match;
 	var extension;
+	var filenameFilter;
 	var urls;
 	var injectableHtml;
+	var replacements = [];
 
 	// eslint-disable-next-line no-cond-assign
 	while (match = pattern.exec(content)) {
 		extension = '.' + match[1];
+		filenameFilter = match[3];
 		urls = [];
 
 		if (!templates[extension]) {
@@ -51,8 +52,13 @@ function injectSources(context, base, files, htmlFile, cb) {
 		// eslint-disable-next-line no-loop-func
 		manifest.forEach(function(file) {
 			var fileExtension = path.extname(file.file.filename());
+			var fileRelativeFilename;
 
-			if (fileExtension === extension) {
+			if (file.file.originalFilename()) {
+				fileRelativeFilename = path.relative(path.join(__root, srcBase), file.file.originalFilename());
+			}
+
+			if (fileExtension === extension || fileRelativeFilename && filenameFilter === fileRelativeFilename) {
 				urls.push(file.url);
 			}
 		});
@@ -63,24 +69,40 @@ function injectSources(context, base, files, htmlFile, cb) {
 
 		// eslint-disable-next-line no-loop-func
 		injectableHtml = urls.map(function(url) {
-			return templates[extension].replace(urlPatterm, url);
+			return templates[extension].replace(urlPattern, url);
 		}).join('');
 
-		content = content.replace(new RegExp(regexSafe(match[0]), 'g'), injectableHtml);
-
-		filesInjected = true;
+		replacements.push({
+			location: [match.index, match.index + match[0].length],
+			what: injectableHtml,
+		});
 	}
 
-	if (filesInjected) {
+	if (replacements.length) {
+		for (var i = replacements.length - 1; i >= 0; i--) {
+			content = [
+				content.slice(0, replacements[i].location[0]),
+				replacements[i].what,
+				content.slice(replacements[i].location[1]),
+			].join('');
+		}
+
 		htmlFile.contents(content);
 	}
 
 	cb(null, files);
 }
 
+function injectSources(context, base, srcBase, files, htmlFiles, cb) {
+	async.eachSeries(htmlFiles, injectSourcesForFile.bind(null, context, base, srcBase, files), function() {
+		cb(null, files);
+	});
+}
+
 module.exports = function(htmlFileGlob, rawOptions) {
 	var options = rawOptions || {};
 	var base = options.base || '';
+	var srcBase = options.srcBase || '';
 
 	if (!htmlFileGlob) {
 		throw new Error('`htmlFileGlob` is not set');
@@ -88,12 +110,12 @@ module.exports = function(htmlFileGlob, rawOptions) {
 
 	return function inject(files, cb) {
 		var context = this;
-		var htmlFile = findHtmlFile(files, htmlFileGlob);
+		var htmlFiles = findHtmlFiles(files, htmlFileGlob);
 
-		if (!htmlFile) {
+		if (!htmlFiles.length) {
 			cb(null, files);
 		} else {
-			injectSources(context, base, files, htmlFile, cb);
+			injectSources(context, base, srcBase, files, htmlFiles, cb);
 		}
 	};
 };
