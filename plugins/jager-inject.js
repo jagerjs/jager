fed28c08f8062ca6016b01199e6fa81509ce1d9e
Tim <me@justim.net>
2017-03-15 15:04:57 +0100

    Add support for more file type in the inject plugin

--- plugins/jager-inject.js
+++ plugins/jager-inject.js
@@ -1,53 +1,68 @@
 
 /**
- * Inject reference to files into a html file
+ * Inject reference to files into a file
+ *
+ * Currently only support for html and css files
  */
 
 'use strict';
 
 var path = require('path');
 
+var _ = require('lodash');
 var async = require('async');
 var minimatch = require('minimatch');
 
 var __root = process.cwd();
 
-var templates = {
-	'.js': '<script src="{url}"></script>',
-	'.css': '<link rel="stylesheet" href="{url}">',
-	'.img': '<img src="{url}">',
-};
-
-function findHtmlFiles(files, glob) {
+function findInjectableFiles(files, glob) {
 	return files.filter(function(file) {
 		return minimatch(file.filename(), glob);
 	});
 }
 
-function injectSourcesForFile(context, base, srcBase, files, htmlFile, cb) {
+function getPatternInfo(file) {
+	if (_.endsWith(file.filename(), '.html')) {
+		return {
+			pattern: /<!-- ?inject:(js|css|img)(\(([^)]+)\))? ?-->/g,
+			filenameFilter: 3,
+			templates: {
+				'.js': '<script src="{url}"></script>',
+				'.css': '<link rel="stylesheet" href="{url}">',
+				'.img': '<img src="{url}">',
+			},
+		};
+	} else if (_.endsWith(file.filename(), '.css')) {
+		return {
+			pattern: /url\(('|")?([^\1]+?)\1\)/g,
+			filenameFilter: 2,
+			templates: 'url({url})',
+		};
+	}
+
+	throw new Error('Unsupported file');
+}
+
+function injectSourcesForFile(context, base, srcBase, files, sourceFile, cb) {
 	var manifest = context.getManifest(base).filter(function(file) {
-		return file.file.filename() !== htmlFile.filename();
+		return file.file.filename() !== sourceFile.filename();
 	});
 
-	var content = htmlFile.contents();
-	var pattern = /<!-- ?inject:(\w+)(\(([^)]+)\))? ?-->/g;
+	var content = sourceFile.contents();
+	var patternInfo = getPatternInfo(sourceFile);
 	var urlPattern = /{url}/g;
 	var match;
 	var extension;
 	var filenameFilter;
 	var urls;
-	var injectableHtml;
+	var injectableContent;
 	var replacements = [];
 
 	// eslint-disable-next-line no-cond-assign
-	while (match = pattern.exec(content)) {
+	while (match = patternInfo.pattern.exec(content)) {
 		extension = '.' + match[1];
-		filenameFilter = match[3];
+		filenameFilter = match[patternInfo.filenameFilter];
 		urls = [];
-
-		if (!templates[extension]) {
-			continue;
-		}
 
 		// eslint-disable-next-line no-loop-func
 		manifest.forEach(function(file) {
@@ -58,7 +73,9 @@ function injectSourcesForFile(context, base, srcBase, files, htmlFile, cb) {
 				fileRelativeFilename = path.relative(path.join(__root, srcBase), file.file.originalFilename());
 			}
 
-			if (fileExtension === extension || fileRelativeFilename && filenameFilter === fileRelativeFilename) {
+			if (patternInfo.templates[extension] && fileExtension === extension
+				|| fileRelativeFilename && filenameFilter === fileRelativeFilename
+			) {
 				urls.push(file.url);
 			}
 		});
@@ -68,13 +85,13 @@ function injectSourcesForFile(context, base, srcBase, files, htmlFile, cb) {
 		}
 
 		// eslint-disable-next-line no-loop-func
-		injectableHtml = urls.map(function(url) {
-			return templates[extension].replace(urlPattern, url);
+		injectableContent = urls.map(function(url) {
+			return (patternInfo.templates[extension] || patternInfo.templates).replace(urlPattern, url);
 		}).join('');
 
 		replacements.push({
 			location: [match.index, match.index + match[0].length],
-			what: injectableHtml,
+			what: injectableContent,
 		});
 	}
 
@@ -87,35 +104,35 @@ function injectSourcesForFile(context, base, srcBase, files, htmlFile, cb) {
 			].join('');
 		}
 
-		htmlFile.contents(content);
+		sourceFile.contents(content);
 	}
 
 	cb(null, files);
 }
 
-function injectSources(context, base, srcBase, files, htmlFiles, cb) {
-	async.eachSeries(htmlFiles, injectSourcesForFile.bind(null, context, base, srcBase, files), function() {
+function injectSources(context, base, srcBase, files, injectableFiles, cb) {
+	async.eachSeries(injectableFiles, injectSourcesForFile.bind(null, context, base, srcBase, files), function() {
 		cb(null, files);
 	});
 }
 
-module.exports = function(htmlFileGlob, rawOptions) {
+module.exports = function(fileGlob, rawOptions) {
 	var options = rawOptions || {};
 	var base = options.base || '';
 	var srcBase = options.srcBase || '';
 
-	if (!htmlFileGlob) {
-		throw new Error('`htmlFileGlob` is not set');
+	if (!fileGlob) {
+		throw new Error('`fileGlob` is not set');
 	}
 
 	return function inject(files, cb) {
 		var context = this;
-		var htmlFiles = findHtmlFiles(files, htmlFileGlob);
+		var injectableFiles = findInjectableFiles(files, fileGlob);
 
-		if (!htmlFiles.length) {
+		if (!injectableFiles.length) {
 			cb(null, files);
 		} else {
-			injectSources(context, base, srcBase, files, htmlFiles, cb);
+			injectSources(context, base, srcBase, files, injectableFiles, cb);
 		}
 	};
 };
