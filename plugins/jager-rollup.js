574d001fc6e85d5f25d19ccb550383b65027b458
Tim <me@justim.net>
2017-03-15 15:46:47 +0100

    Use rollup-es2015 preset

--- plugins/jager-rollup.js
+++ plugins/jager-rollup.js
@@ -7,14 +7,9 @@ var babel = require('rollup-plugin-babel');
 var nodeResolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
 
-var modifyBabelPreset = require('modify-babel-preset');
-var babelPresetEs2015 = modifyBabelPreset('es2015', {
-	// remove commonjs transform
-	'transform-es2015-modules-commonjs': false,
-});
+var babelPresetEs2015 = require('babel-preset-es2015-rollup');
 var babelPresetReact = require('babel-preset-react');
 var babelPresetStage0 = require('babel-preset-stage-0');
-var babelPluginExternalHelpers = require('babel-plugin-external-helpers');
 
 var gatedMap = require('./../lib/gated-map');
 
@@ -31,9 +26,6 @@ function rollupFile(options, context, file, cb) {
 					babelPresetEs2015,
 					babelPresetReact,
 					babelPresetStage0,
-				],
-				plugins: [
-					babelPluginExternalHelpers,
 				],
 			}),
 			nodeResolve({
